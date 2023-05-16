2f634ddc440b0a664011c421294d230af076811e
manuelgloeckler <manu.gloeckler@hotmail.de>
2023-05-16 09:57:21 +0200

    last experiment done

--- figures/appendix_mc_approximaiton/run_exact_FIM_vs_APPROX.sh
+++ figures/appendix_mc_approximaiton/run_exact_FIM_vs_APPROX.sh
@@ -2,5 +2,5 @@
 
 
 rbibm +experiment=appendix_vae & rbibm +experiment=appendix_vae1  & rbibm +experiment=appendix_vae2
-rbibm +experiment=eval_l2 "idx=range(0,24)"
+rbibm +experiment=eval_l2 "idx=range(0,24)" name=vae
 
